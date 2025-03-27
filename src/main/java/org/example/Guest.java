c9c7ff73792540ac5f243189e05c9020ab37b023
João Antonio Lassister Melo <joao.lassister@sou.unifal-mg.edu.br>
2025-03-27 11:38:27 -0300

    Primeiro commit

---
+++ src/main/java/org/example/Guest.java
@@ -0,0 +1,86 @@
+package org.example;
+
+public class Guest {
+    String title;
+    String firstName;
+    String lastName;
+    String address;
+    String zipCode;
+    String state;
+    String country;
+    String dateOfBirth;
+    String email;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFirstname() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Guest xyz = new Guest();
+        xyz.setTitle("Mister");
+
+        String resposta = xyz.getTitle();
+        System.out.println(resposta);
+
+        xyz.setFirstName("Atticus");
+
     }
 }
